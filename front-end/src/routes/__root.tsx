4e059ae8b5c8e3a34c7eb2e054873963203407bd
ajjmacias@addu.edu.ph <ajjmacias@addu.edu.ph>
2025-08-18 16:29:09 +0800

    feat: added events route

--- front-end/src/routes/__root.tsx
+++ front-end/src/routes/__root.tsx
@@ -4,12 +4,15 @@ import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 export const Route = createRootRoute({
   component: () => (
     <>
-      <div className="p-2 flex gap-2">
+      <div className="p-2 flex gap-4">
         <Link to="/" className="[&.active]:font-bold">
           Home
         </Link>{" "}
         <Link to="/about" className="[&.active]:font-bold">
           About
+        </Link>
+        <Link to="/create" className="[&.active]:font-bold">
+          Events{" "}
         </Link>
       </div>
       <hr />
