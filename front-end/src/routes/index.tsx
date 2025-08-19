977a347cb38ae0ad37acc8d00bf4d61729f4c510
ajjmacias@addu.edu.ph <ajjmacias@addu.edu.ph>
2025-08-19 11:09:50 +0800

    feat: login integration for backend

--- front-end/src/routes/index.tsx
+++ front-end/src/routes/index.tsx
@@ -1,58 +1,80 @@
 import { Authenticator } from "@aws-amplify/ui-react";
 import "../amplifyClient";
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import { getCurrentUser } from "aws-amplify/auth";
 import "@aws-amplify/ui-react/styles.css";
 import { fetchAuthSession } from "aws-amplify/auth";
 import "../App.css";
 import { createFileRoute } from "@tanstack/react-router";
+import axios from "axios";
 
 export const Route = createFileRoute("/")({
   component: Index,
 });
 
+/*
 async function fetchTodos() {
   const session = await fetchAuthSession();
   const token = session.tokens?.idToken?.toString();
-
-  const res = await fetch(import.meta.env.VITE_API_URL, {
+  console.log(token);
+  const res = await fetch(`${import.meta.env.VITE_API_URL}/items`, {
     headers: { Authorization: token ? `Bearer ${token}` : `` },
   });
   if (!res.ok) {
     throw new Error("failed fetch");
   }
-
   return res.json();
-}
+}*/
 
-async function getDetails() {
-  const { username, userId, signInDetails } = await getCurrentUser();
-  console.log(username);
-}
+async function loginUser() {}
 
+/*
 function useItemsQuery() {
   return {
     queryKey: ["todos"],
     queryFn: fetchTodos,
   };
-}
+}*/
 
 function Index() {
-  const { queryKey, queryFn } = useItemsQuery();
-  const { data, isFetching, refetch } = useQuery({ queryKey, queryFn });
+  const mutation = useMutation({
+    mutationFn: async () => {
+      const session = await fetchAuthSession();
+      const token = session.tokens?.idToken?.toString();
+      const { username } = await getCurrentUser();
 
-  console.log(data);
+      return axios.post(
+        `${import.meta.env.VITE_API_URL}/users`,
+        { PK: username, SK: username },
+        {
+          headers: {
+            Authorization: token ? `Bearer ${token}` : "",
+          },
+        }
+      );
+    },
+    onSuccess: () => {
+      close();
+    },
+  });
+
+  const handleLogin = () => {
+    mutation.mutate();
+  };
 
   return (
     <Authenticator socialProviders={["google"]}>
-      {({ signOut, user }) => (
-        <main>
-          <h1 className="text-red-800">Hello {user?.username}</h1>
-          <button onClick={() => refetch()}> Refetch </button>
-          <button onClick={signOut}>Sign out</button>
-          {data && <div>{JSON.stringify(data, null, 2)}</div>}
-        </main>
-      )}
+      {({ signOut, user }) =>
+        user ? (
+          <main>
+            <button onClick={() => handleLogin()}> Login </button>
+
+            <button onClick={signOut}>Sign out</button>
+          </main>
+        ) : (
+          <div>Loading...</div>
+        )
+      }
     </Authenticator>
   );
 }
