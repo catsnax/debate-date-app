e1090f0f74c6e71824ce8f09245bec56f9434642
catsnax <aaron.justin.macias@gmail.com>
2025-08-20 13:50:02 +0800

    feat: api integration with /events get request

--- front-end/src/routes/events.tsx
+++ front-end/src/routes/events.tsx
@@ -1,12 +1,30 @@
 import { createFileRoute } from "@tanstack/react-router";
 import Card from "../components/card";
 import EventForm from "../components/EventForm";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 
 export const Route = createFileRoute("/events")({
   component: RouteComponent,
 });
 
+async function fetchEvents() {
+  const res = await fetch(`${import.meta.env.VITE_API_URL}/events`);
+  if (!res.ok) {
+    throw new Error("failed fetch");
+  }
+  return res.json();
+}
+
 function RouteComponent() {
+  const queryClient = useQueryClient();
+
+  const { data, isFetching } = useQuery({
+    queryKey: ["events"],
+    queryFn: fetchEvents,
+  });
+
+  console.log(data);
+
   return (
     <>
       <EventForm />
